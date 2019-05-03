a51d1b0aa9e5d759211c3f32e36458da3b83a21d
Reinier Blanco <reinierblanco10@gmail.com>
2019-05-03 18:17:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function shout(string) {
 
 
 function whisper(string) {
-  return string.toLowerCase() === toLowerCase;
+  return string.toLowerCase("HELLO!") === toLowerCase;
 }
 
 function logShout(string) {
