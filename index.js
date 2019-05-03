37065418f289ff1079d4c502fe4e67ce45e03fd2
Reinier Blanco <reinierblanco10@gmail.com>
2019-05-03 18:04:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ function shout(string) {
 
 
 function whisper(string) {
-  //return string.toLowerCase()
+  
 }
 
 function logShout(string) {
-  //console.log(string.toUpperCase() );
+  
 }
 
 function logWhisper(string) {
-   //console.log(string.toLowerCase() );
+  
 }
 
 function sayHiToGrandma(string) {
